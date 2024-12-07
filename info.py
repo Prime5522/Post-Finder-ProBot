aa1be8bf5b783bb805783912300858bd07d5699d
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-12-07 22:42:44 +0600

    Update info.py

--- info.py
+++ info.py
@@ -7,8 +7,8 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", "20632324"))
-API_HASH     = environ.get("API_HASH", "7472998b241dd149fc2b2167ce045c0e")
+API_ID       = int(environ.get("API_ID", ""))
+API_HASH     = environ.get("API_HASH", "")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002184630716"))
